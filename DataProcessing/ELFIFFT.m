99d08b73851494a3f5365ecfa1ee9b96fd0d80fa
jcalabro <jcalabro@umass.edu>
2015-09-25 11:17:25 -0400

    Updates scripts, removes old one, and updates .gitignore

--- DataProcessing/ELFIFFT.m
+++ DataProcessing/ELFIFFT.m
@@ -13,55 +13,43 @@ function [] = ELFIFFT()
     pattern = fullfile(directory,'*.set');
     allFilesInDirectory = dir(pattern);
     setFiles = removeDotUnderscores(allFilesInDirectory);
-    numSubjects = size(setFiles); 
+    numSubjects = size(setFiles);
     
     % Find the average
-    for subjectIndex = 1 : 1
+    for subjectIndex = 1 : numSubjects
         EEG = pop_loadset('filename', setFiles{subjectIndex}, 'filepath', directory);
-        concatenatedEEGs(subjectIndex, :) = EEG;
-%         [ym, f] = fourieeg(EEG,channels,[],0,7);
-%         CombinedFiles(subjectIndex,:) = ym;
+        EEG = epoch2continuous(EEG);
+        [ym, f] = fourieeg(EEG,channels,[],0,7);
+        CombinedFiles(subjectIndex,:) = ym;
     end
-    
-    disp(EEG.data);
-    
-    save subject.mat
-    
-%     for i = 1 : 6500
-%         disp(EEG.data(1, i, 1));
-%     end
 
+    AveResponse = mean(CombinedFiles,1);
 
-
-%     [ym, f] = fourieeg(concatenatedEEGs,channels,[],0,7);
-%     CombinedFiles(subjectIndex,:) = ym;
-% 
-%     AveResponse = mean(CombinedFiles,1);
-% 
-%     BaseSignal = AveResponse(100); % Bin 100 is 6.04
+    BaseSignal = AveResponse(57); % Bin 100 is 6.04
 %     bnoise = [AveResponse(90:99),AveResponse(101:110)];
-%     BaseNoise = mean(bnoise);
-%     BaseRatio = BaseSignal/BaseNoise;
-%     BaseSNR = mean(BaseRatio);
-% 
-%     OddSignal = AveResponse(21); % Bin 21 is 1.22
-%     onoise = [AveResponse(11:20),AveResponse(22:31)];
-%     OddNoise = mean(onoise);
-%     OddRatio = OddSignal/OddNoise;
-%     OddSNR = mean(OddRatio);
-% 
-%     disp(BaseSNR);
-%     disp(OddSNR);
-% 
-%     plot(f,AveResponse);
-%     % axis([1 7 0 35]); % Change the last number to adjust y-scale
-%     xlim([1 7]);
-%     ylim auto
-%     xlabel('Frequency (Hz)')
-%     ylabel('Y(f)')
-%     title('Concatenated')
+    bnoise = [AveResponse(37:46),AveResponse(47:57)];
+    BaseNoise = mean(bnoise);
+    BaseRatio = BaseSignal/BaseNoise;
+    BaseSNR = mean(BaseRatio);
+
+    OddSignal = AveResponse(21); % Bin 21 is 1.22
+    onoise = [AveResponse(11:20),AveResponse(22:31)];
+    OddNoise = mean(onoise);
+    OddRatio = OddSignal/OddNoise;
+    OddSNR = mean(OddRatio);
     
-%     save test.mat;
+    save finishedData.mat;
+
+    disp(BaseSNR);
+    disp(OddSNR);
+
+    plot(f,AveResponse);
+    % axis([1 7 0 35]); % Change the last number to adjust y-scale
+    xlim([1 7]);
+    ylim auto
+    xlabel('Frequency (Hz)')
+    ylabel('Y(f)')
+    
 end
 
 function fileNames = removeDotUnderscores(setFiles)
