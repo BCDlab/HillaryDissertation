4f4783f9280ca4e5e8295ba718071113e2ef44be
jcalabro <jcalabro@umass.edu>
2015-09-28 16:00:34 -0400

    Pushing progress

--- DataProcessing/ELFIFFT.m
+++ DataProcessing/ELFIFFT.m
@@ -1,11 +1,13 @@
-function [] = ELFIFFT()
-%         averager( ALLEEG , 'Criterion', 1, 'DSindex',  5, 'Stdev', 'on');
-
+function [] = ELIFFT()
     % Prompt the user for the condition and channels with default values
     prompt = {'Condition','Channels'};
-    defaults = {'LabelPre','75'};
+    defaults = {'LabelPre','75', ''};
     promptResponse = inputdlg(prompt,'Condition',1,defaults);
     [condition, channels] = deal(promptResponse{:});
+
+    %TODO: Exclude Subjects
+    % The subjects to be excluded from the calculations
+    %     excludedSubjects = {'5', '11'};
 
     % Prompt the user for the path to the .set files and find all of that
     % directory's .set files. Also store the number of subjects
@@ -13,44 +15,63 @@ function [] = ELFIFFT()
     pattern = fullfile(directory,'*.set');
     allFilesInDirectory = dir(pattern);
     setFiles = removeDotUnderscores(allFilesInDirectory);
-    numSubjects = size(setFiles);
-    
+    %     setFiles = removeExcludedSubjects(setFiles, excludedSubjects);
+
     % Find the average
-    for subjectIndex = 1 : numSubjects
-        EEG = pop_loadset('filename', setFiles{subjectIndex}, 'filepath', directory);
+    for subjectIndex = 1 : size(setFiles)
+        EEG = pop_loadset('filename', setFiles{subjectIndex}{1}, 'filepath', directory);
         EEG = epoch2continuous(EEG);
         [ym, f] = fourieeg(EEG,channels,[],0,7);
         CombinedFiles(subjectIndex,:) = ym;
     end
 
-    AveResponse = mean(CombinedFiles,1);
+    % AveResponse = mean(CombinedFiles,1);
 
-    BaseSignal = AveResponse(57); % Bin 100 is 6.04
-%     bnoise = [AveResponse(90:99),AveResponse(101:110)];
-    bnoise = [AveResponse(37:46),AveResponse(47:57)];
-    BaseNoise = mean(bnoise);
-    BaseRatio = BaseSignal/BaseNoise;
-    BaseSNR = mean(BaseRatio);
+    save function.mat;
 
-    OddSignal = AveResponse(21); % Bin 21 is 1.22
-    onoise = [AveResponse(11:20),AveResponse(22:31)];
-    OddNoise = mean(onoise);
-    OddRatio = OddSignal/OddNoise;
-    OddSNR = mean(OddRatio);
-    
-    save finishedData.mat;
+    %     BaseSignal = AveResponse(100); % Bin 100 is 6.04
+    %     bnoise = [AveResponse(90:99),AveResponse(101:110)];
 
-    disp(BaseSNR);
-    disp(OddSNR);
-
-    plot(f,AveResponse);
-    % axis([1 7 0 35]); % Change the last number to adjust y-scale
-    xlim([1 7]);
-    ylim auto
-    xlabel('Frequency (Hz)')
-    ylabel('Y(f)')
-    
+    %     BaseSignal = AveResponse(57);
+    %     bnoise = [AveResponse(37:46),AveResponse(47:57)];
+    %     BaseNoise = mean(bnoise);
+    %     BaseRatio = BaseSignal/BaseNoise;
+    %     BaseSNR = mean(BaseRatio);
+    % 
+    %     OddSignal = AveResponse(21); % Bin 21 is 1.22
+    %     onoise = [AveResponse(11:20),AveResponse(22:31)];
+    %     OddNoise = mean(onoise);
+    %     OddRatio = OddSignal/OddNoise;
+    %     OddSNR = mean(OddRatio);
+    %     
+    %     save finishedData.mat;
+    % 
+    %     disp(BaseSNR);
+    %     disp(OddSNR);
+    % 
+    %     plot(f,AveResponse);
+    %     % axis([1 7 0 35]); % Change the last number to adjust y-scale
+    %     xlim([1 7]);
+    %     ylim auto
+    %     xlabel('Frequency (Hz)')
+    %     ylabel('Y(f)')
 end
+
+%TODO: Fix this
+% function finalSubjects = removeExcludedSubjects(allSubjects, excludedSubjects)
+%     excludedSize = size(excludedSubjects);
+%     finalSubjectIndex = 1;
+%     for excludedIndex = 1 : excludedSize(2)
+%         allSize = size(allSubjects);
+%         for allIndex = 1 : allSize(1)
+%             currentExcludedSize = size(excludedSubjects{excludedIndex});
+%             if ~strcmp(strcat('ELFI_', excludedSubjects{excludedIndex}, '_'), allSubjects{allIndex}{1}(1:(6 + currentExcludedSize(2))))
+%                 finalSubjects{finalSubjectIndex} = allSubjects{allIndex}{1};
+%                 finalSubjectIndex = finalSubjectIndex + 1;
+%             end
+%         end
+%     end
+% end
 
 function fileNames = removeDotUnderscores(setFiles)
     setFilesWithoutDotUnderscores = cell(size(setFiles));
